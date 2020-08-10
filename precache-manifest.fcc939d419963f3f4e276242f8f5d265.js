2e0159cda87ffbf46e1c91d5f51a9bec4981daff
dj <dman992266@gmail.com>
2020-08-10 06:48:20 -0400

    Updates

---
+++ precache-manifest.fcc939d419963f3f4e276242f8f5d265.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93da2746c18bd7f9fc331fdcf9e0828",
+    "url": "/react-auth0/index.html"
+  },
+  {
+    "revision": "8f5197724d195aa9c9f5",
+    "url": "/react-auth0/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b8be1869ce2e62a0183b",
+    "url": "/react-auth0/static/css/main.ce5a5663.chunk.css"
+  },
+  {
+    "revision": "8f5197724d195aa9c9f5",
+    "url": "/react-auth0/static/js/2.a5f4bd0d.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-auth0/static/js/2.a5f4bd0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8be1869ce2e62a0183b",
+    "url": "/react-auth0/static/js/main.04dd7af4.chunk.js"
+  },
+  {
+    "revision": "f8a3a4d7810eff67078d",
+    "url": "/react-auth0/static/js/runtime-main.1b6461b5.js"
+  },
+  {
+    "revision": "506f608b85b31a7c372a6cde1801166d",
+    "url": "/react-auth0/static/media/teeLogin.506f608b.png"
+  }
+]);
